bb96f07e0ce3378636e766d2e3f665c591d43e56
rotgamedev <rotgamedev@gmail.com>
2021-03-20 11:04:53 +0100

    Update sketch_feb22a.ino
    web page

--- sketch_feb22a.ino
+++ sketch_feb22a.ino
@@ -1,5 +1,7 @@
 #include <FS.h>
 #include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
 #include <ArduinoJson.h>
 #include <WiFiManager.h>
 #include <PubSubClient.h>
@@ -17,6 +19,10 @@
 WiFiClient espClient;
 PubSubClient client(espClient);
 WiFiManager wifiManager;
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
 
 //Variable for mqtt
 char friendly_name[40];
@@ -707,7 +713,7 @@ void setup() {
   {
     MoistureSensorsRead();
   }*/
-  
+  server.begin();
   Serial.println("End setup");
 }
 
@@ -937,6 +943,8 @@ void loop()
 
     CheckMQTTState();
   }
+
+  WebPage();
 
   if(needStabilization)
   {
@@ -1325,4 +1333,103 @@ void ShowSavedFlowerConfigOled()
     display.display();
     delay(5000);
   }  
+}
+
+void WebPage()
+{
+  WiFiClient client = server.available();   // Listen for incoming clients
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("GPIO 5 on");
+              //output5State = "on";
+              //digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("GPIO 5 off");
+              //output5State = "off";
+              //digitalWrite(output5, LOW);
+            } else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("GPIO 4 on");
+              //output4State = "on";
+             // digitalWrite(output4, HIGH);
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("GPIO 4 off");
+              //output4State = "off";
+             // digitalWrite(output4, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"stylesheet\" href=\"https://use.fontawesome.com/releases/v5.7.2/css/all.css\" integrity=\"sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr\" crossorigin=\"anonymous\">");                          
+
+            client.println("<style>html { font-family: Arial; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println("h1 { font-size: 3.0rem;}");    
+            client.println("h2 { font-size: 2.0rem;}");
+            client.println("p { font-size: 2.0rem;}");
+            client.println(".units { font-size: 1.2rem;}");
+            client.println(".labels{ font-size: 1.5rem; vertical-align:middle; padding-bottom: 15px;}</style></head>");
+           
+            // Web Page Heading
+            client.println("<body><h1>Watering Station</h1>");
+            client.println("<h2>Air:</h2>");      
+            client.println("<p><i class=\"fas fa-sun\" style=\"color:#f2f20d;\"></i> <span class=\"labels\">Light:</span> <span>"+(String)lux+"</span> <sup class=\"units\">lx</sup></p>");
+            client.println("<p><i class=\"fas fa-thermometer-half\" style=\"color:#059e8a;\"></i> <span class=\"labels\">Temperature:</span> <span>"+(String)temp+"</span> <sup class=\"units\">&deg;C</sup></p>");
+            client.println("<p><i class=\"fas fa-tint\" style=\"color:#00add6;\"></i> <span class=\"labels\">Humidity:</span> <span>"+(String)humidity+"</span> <sup class=\"units\">&percnt;</sup></p>");
+            client.println("<h2>Flowers:</h2>");
+       
+            int activeValue[4]={atoi(f1_active), atoi(f2_active), atoi(f3_active), atoi(f4_active)};
+            String flowerNames[4]={String(f1_name),String(f2_name),String(f3_name),String(f4_name)};
+    
+            for (int i = 0; i <= 3; i++)
+            {
+              if(activeValue[i]==1)
+              {
+                client.println("<p><i class=\"fas fa-seedling\" style=\"color:#00cc00;\"></i> <span>"+String(flowerNames[i])+"</span> <span class=\"labels\">Moisture:</span> <span>"+(String)soilMoisturePercent[i]+"</span> <sup class=\"units\">&percnt;</sup></p>");
+              }
+            }
+            client.println("</body></html>");
+            //client.println("<p><i class=\"fas fa-seedling"\" style=\"color:#00cc00;\"></i> <span>"+String(f1_name)+"</span> <span class=\"labels\"> - Moisture:</span> <span>"+(String)soilMoisturePercent[0]+"</span> <sup class=\"units\">&percnt;</sup></p>");
+            //client.println("<p><i class=\"fas fa-seedling"\" style=\"color:#00cc00;\"></i> <span>"+String(f2_name)+"</span> <span class=\"labels\"> - Moisture:</span> <span>"+(String)soilMoisturePercent[1]+"</span> <sup class=\"units\">&percnt;</sup></p>");
+            //client.println("<p><i class=\"fas fa-seedling"\" style=\"color:#00cc00;\"></i> <span>"+String(f3_name)+"</span> <span class=\"labels\"> - Moisture:</span> <span>"+(String)soilMoisturePercent[2]+"</span> <sup class=\"units\">&percnt;</sup></p>");
+            //client.println("<p><i class=\"fas fa-seedling"\" style=\"color:#00cc00;\"></i> <span>"+String(f4_name)+"</span> <span class=\"labels\"> - Moisture:</span> <span>"+(String)soilMoisturePercent[3]+"</span> <sup class=\"units\">&percnt;</sup></p>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
